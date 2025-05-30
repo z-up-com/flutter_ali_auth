ff6bcc153d3bbf4a42c38e05dd7f2e3c3624f608
Rain <huangyulu81@gmail.com>
2025-05-30 14:07:17 +0800

    切换其他方式时不需要勾选

--- ios/Classes/AliAuthPlugin.m
+++ ios/Classes/AliAuthPlugin.m
@@ -358,7 +358,7 @@ bool bool_false = false;
               } else if ([PNSCodeLoginControllerClickChangeBtn isEqualToString:code]) {
                 // 通过switchCheck 参数动态控制 是否需要切换其他方式时需要勾选
                 NSDictionary *dic = self -> _callData.arguments;
-                if (!self->_isChecked && !self-> _isHideToast && [dic boolValueForKey: @"switchCheck" defaultValue: YES]) {
+                if (!self->_isChecked && !self-> _isHideToast && [dic boolValueForKey: @"switchCheck" defaultValue: NO]) {
                   [self showToast: [dic stringValueForKey: @"toastText" defaultValue: @"请先阅读用户协议"]];
                   return;
                 } else {
